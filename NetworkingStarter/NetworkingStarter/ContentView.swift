ffcc726c7f1fda0f28546de8345e69900dd1edf7
Emiliano Magana <websmithmxl@gmail.com>
2025-12-01 13:01:12 -0800

    Fixed typos

--- NetworkingStarter/NetworkingStarter/ContentView.swift
+++ NetworkingStarter/NetworkingStarter/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = PostsViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack
+        {
+            Group
+            {
+                
+            }
         }
-        .padding()
     }
 }
 
